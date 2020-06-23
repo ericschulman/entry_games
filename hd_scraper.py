9bf132a919d778862182d1361656a5195d549abf
Eric Schulman <ericschulman@utexas.edu>
2020-06-23 14:54:15 -0500

    improved comments

--- hd_scraper.py
+++ hd_scraper.py
@@ -20,12 +20,12 @@ class HomeDepotScraper(GenericScraper):
 
 
         #get the html element - try to under stand what this line is doing
-        state_element = driver.find_element(By.XPATH, '(//*[@class="stateList__item"])[1]') 
-        #to fully understand the previous line, you will have to under stand what the xpath query is doing
-        # i.e. the xpath query is the second argument "//*[@class="stateList__item"])[1]'"
-        #what does [1] do?
-        #what does * do?
-        #what does @class do?
+        state_xpath_query = '(//*[@class="stateList__item"])[1]' #xpath query to the first state on the page
+        state_element = driver.find_element(By.XPATH, state_xpath_query) 
+        #to fully understand the previous line, you will have to understand how I am querying the webpage
+        #what does [1] do in line 23?
+        #what does * do in line 23?
+        #what does @class do in line 23?
 
         state_html = state_element.get_attribute('innerHTML') #get the html text from the xpath element
         state_tree = etree.fromstring(state_html) #parse it as a tree
