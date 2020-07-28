890ba128a0fa581be156ae8b5b84e3c88850430a
Eric Schulman <ericschulman@utexas.edu>
2020-07-28 16:20:54 -0500

    setup scraper for new tables

--- scraper.py
+++ scraper.py
@@ -7,8 +7,11 @@ import time, datetime
 
 #dealing with the database
 import sqlite3
+import pandas as pd
 
-#parsing html
+#parsing html/json/requests
+import json
+import requests
 import lxml
 from lxml import etree
 from lxml import html
@@ -38,17 +41,101 @@ class GenericScraper:
         self.drivers.append(driver)
 
 
-    def db_create(self,db):
+    def check_table(self, cur, tablename):
+        """check to see if table exists"""
+        query = ''' SELECT count(name) 
+                    FROM sqlite_master 
+                    WHERE type='table' 
+                    AND name='%s' '''%tablename
+        cur.execute(query)
+        if cur.fetchone()[0] == 1:
+            return True
+        return False
+
+
+    def get_censusdata(self,fields):
+        """load formatted fields from census api"""
+        api_query = "https://api.census.gov/data/2018/acs/acs5?&get=NAME"
+        print(fields)
+        for field in fields:
+            api_query = api_query + "," + field
+        api_query = api_query + "&for=place:*&in=state:*"
+        print(api_query)
+        #call the API and collect the response
+        response = requests.get(api_query)
+        #load the response into a JSON, ignoring the first element which is just field labels
+        formattedResponse = json.loads(response.text)
+        return formattedResponse
+
+
+    def load_censusdata(self,db,censusfile):
+        """add hardcoded census data to the database
+        as a new table. drop table if already exists"""
+
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+
+        #check to see if table exists 
+        if self.check_table(cur,"census"):
+            cur.execute("drop table census") #if the table exists, drop it
+        
+        #get state data and save as a dataframe
+        censusFielddata = pd.read_csv(censusfile)
+        census_codes = list(censusFielddata["census_code"])
+        #censusdata = self.get_censusdata(census_codes)
+
+        #note: that there are 12 codes in columns.csv, 
+        #but the api returns rows with 14 fields
+        #the last two feilds are the state and place code
+
+        #note: not all feilds have data...
+
+
+        #create a query that creates a table with the columns names
+        fieldnames = list(censusFielddata["column"]) #these are the names you should use
+        #create a query that inserts into the new table
+
+        #commit changes to db
+        conn.commit()
+
+
+    def load_statedata(self,db,statefile):
+        """add hardcoded state data to the database
+        as a new table"""
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+
+        #check to see if table exists
+        if self.check_table(cur,"states"):
+            cur.execute("drop table states") #if the table exists, drop it
+        #get state data and save as a dataframe
+        statedata = pd.read_csv(statefile)
+
+        #create a query that creates a table with the columns names
+        fieldnames = list(statedata.columns) #these are the names you should use
+        #create a query that inserts into the new table
+
+        #commit changes to db
+        conn.commit()
+
+
+    def entry_create(self,db):
         """run the sql file to create the db"""
         f = open("db/db_create.sql","r")
         sql = f.read()
-        if (os.path.isfile(db) ):
-            os.remove(db_name)
-        con = sqlite3.connect(db) #create the db
-        cur = con.cursor()
+        conn = sqlite3.connect(db) #create the db
+        cur = conn.cursor()
         cur.executescript(sql)
-        con.commit()
+        conn.commit()
 
+
+    def db_create(self,db):
+        censusfile = "db/columns.csv"
+        statefile = "db/states.csv"
+        if not os.path.isfile(db):
+            self.entry_create(db)
+        self.load_statedata(db,statefile)
+        self.load_censusdata(db,censusfile)
 
 
     def __init__(self, url="", store_directory ="", store="", config_file="config.ini",
@@ -78,16 +165,15 @@ class GenericScraper:
         self.drivers = []
         self.num_drivers = num_drivers
 
+        #create the database
+        self.db_create(self.db)
+
         #initialize web browsers called 'drivers' to do the scrape
         for i in range(self.num_drivers):
             print("------ initializing %s"%self.store)
             self.add_driver()
         
-        #create the database if it is not there
-        if not os.path.isfile(self.db) :
-            self.db_create(self.db)
-
-
+        
     def get_driver(self):
         return self.drivers[-1]
 
@@ -132,10 +218,11 @@ class GenericScraper:
 
         conn.commit()
 
+
     def search_xpath(self,tree,query):
         """hacky way to parse xpath takes an etree as an arguement"""
         return tree.xpath("//*[contains(text(), '%s') or @*[contains(., '%s')]]"%(query,query))
 
 
-
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    scrap = GenericScraper(num_drivers=0)
