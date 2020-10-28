1a88b5d8876b1f8957b0b7740ac5056e8733e570
Eric Schulman <ericschulman@utexas.edu>
2020-10-28 11:59:11 -0500

    aded outer join

--- db/summary_stats.sql
+++ db/summary_stats.sql
@@ -16,13 +16,13 @@ FROM entry, states
 WHERE entry.state = states.STATE_NAME
 OR entry.state = states.STUSAB), 
 
-census_desc as (SELECT * FROM census ORDER BY population DESC),
+census_asc as (SELECT * FROM census ORDER BY population ASC),
 
 census_join AS (SELECT * FROM entry_states
-LEFT JOIN census_desc AS census ON instr(census.name, entry_states.city) >= 1 
+LEFT JOIN census_asc AS census ON instr(census.name, entry_states.city) >= 1 
 and instr(census.name, entry_states.STATE_NAME) >= 1
 GROUP BY address
-ORDER BY population DESC),
+ORDER BY population ASC),
 
 hd_no_dups AS (SELECT * FROM entry_states WHERE store='HD' 
 	GROUP BY address,zipcode), 
@@ -34,11 +34,28 @@ hd AS (SELECT count(*) as HD, city, state
 	FROM hd_no_dups group by city, state order by state,city), 
 
 lo AS (SELECT count(*) as LO, city, state  
-	FROM lo_no_dups group by city, state order by state,city)
+	FROM lo_no_dups group by city, state order by state,city),
 	
-SELECT * FROM hd
+hd_join AS (SELECT  hd.city, STUSAB, HD, LO, 
+place, name, zipcode, income_per_capita, population, 
+under44_1, under44_2, under44_3, older65_1, older_65_2 
+FROM hd
 LEFT JOIN lo ON hd.city = lo.city
 AND hd.state = lo.state
 LEFT JOIN census_join ON hd.city = census_join.city
 AND hd.state = census_join.STATE 
-GROUP BY hd.city, hd.state
+GROUP BY hd.city, hd.state),
+
+lo_join AS ( SELECT  lo.city, STUSAB, HD, LO, 
+place, name, zipcode, income_per_capita, population, 
+under44_1, under44_2, under44_3, older65_1, older_65_2  
+FROM lo
+LEFT JOIN hd ON lo.city = hd.city
+AND lo.state = hd.state
+LEFT JOIN census_join ON lo.city = census_join.city
+AND lo.state = census_join.STATE 
+GROUP BY lo.city, lo.state)
+
+SELECT * FROM (SELECT * FROM hd_join 
+UNION 
+SELECT * FROM lo_join)
